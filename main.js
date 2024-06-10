6f30cfb9eae287f5209e042396605e114f8a9e6d
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-10 20:34:03 +0800

    6.10 nii hicheel deerhiig zassan

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ let gerlesenesekh = true; // boolean:true/false
 // console.log("таны ажил: ", job);
 // console.log("таны анги: ", angi);*/
 
+//daalgavar 1
 //comments make code readable
+
+//daalgavar 2
 let age = 60;
 console.log("таны нас: ", age);
 
+//daalgavar 3
 let FirstName = "Erdenesuren";
 let LastName = "Batbaatar";
 let Country = "Mongolia";
@@ -22,6 +26,7 @@ console.log("таны овог: ", LastName);
 console.log("таны харьялал: ", Country);
 console.log("таны цахим шуудан: ", EMail);
 
+//daalgavar 4
 let studiedYear = 30;
 console.log(typeof studiedYear);
 console.log("таны сурсан жил: ", studiedYear);
@@ -32,29 +37,41 @@ let studiedGra = true; // boolean:true/false
 console.log(typeof studiedGra);
 console.log("таны төгссөн эсэх: ", studiedGra);
 
+//daalgavar 5
 let number1 = 16;
 let number2 = 31;
 niilber = number1 + number2;
 yalgavar = number1 - number2;
 urjver = number1 * number2;
 noogdvor = number1 / number2;
-console.log(niilber);
-console.log(yalgavar);
-console.log(urjver);
-console.log(noogdvor);
+console.log("16 болон 31 ийн нийлбэр:", niilber);
+console.log("16 болон 31 ийн ялгавар:", yalgavar);
+console.log("16 болон 31 ийн үржвэр:", urjver);
+console.log("16 болон 31 ийн ноогдвор:", noogdvor);
 
+//daalgavar 6
 let a = 3;
+console.log("гурвалжины а тал:", a);
 let b = 4;
+console.log("гурвалжины б тал:", b);
 let c = 5;
+console.log("гурвалжины в тал:", c);
 p = a + b + c;
-console.log(p);
+console.log("гурвалжны периметр:", p);
 
-let byte = "8 byte";
+//daalgavar 7
+let byte = "8 bit";
+console.log("1 байт:", byte);
 let kilobyte = "1024 byte";
+console.log("1 килобайт:", kilobyte);
 let megabyte = "1024 kilobyte";
-megabyte = 1024 * 1024 * 8;
-console.log(15 * megabyte);
+console.log("1 мэгабайт:", megabyte);
+let gigabyte = "1024 megabyte";
+console.log("1 гигабайт:", gigabyte);
+gigabyte = 1024 * 1024 * 1024 * 8;
+console.log("15 гигабайт дах битийн тоо хэмжээ:", 15 * gigabyte);
 
+//daalgavar 8
 let CDegree = 35;
 FDegree = CDegree + 32;
 console.log(FDegree);
