53955e903f9012fb16e5227f8efc760f72f4799e
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-10 12:47:15 +0800

    6.10 nii udriin JS

---
+++ main.js
@@ -0,0 +1,64 @@
+/*let ner = "Erdenesuren"; //string
+// let nas = 30; // number 20, 20.30
+let gerlesenesekh = true; // boolean:true/false
+// let job;
+// let angi = null;
+// console.log("таны нэр: ", ner);
+// console.log("таны нас: ", nas);
+// console.log("таны гэрлэсэн эсэх: ", gerlesenesekh);
+// console.log("таны ажил: ", job);
+// console.log("таны анги: ", angi);*/
+
+//comments make code readable
+let age = 60;
+console.log("таны нас: ", age);
+
+let FirstName = "Erdenesuren";
+let LastName = "Batbaatar";
+let Country = "Mongolia";
+let EMail = null;
+console.log("таны нэр: ", FirstName);
+console.log("таны овог: ", LastName);
+console.log("таны харьялал: ", Country);
+console.log("таны цахим шуудан: ", EMail);
+
+let studiedYear = 30;
+console.log(typeof studiedYear);
+console.log("таны сурсан жил: ", studiedYear);
+let studiedUni = "NUM";
+console.log(typeof studiedUni);
+console.log("таны сурсан сургууль: ", studiedUni);
+let studiedGra = true; // boolean:true/false
+console.log(typeof studiedGra);
+console.log("таны төгссөн эсэх: ", studiedGra);
+
+let number1 = 16;
+let number2 = 31;
+niilber = number1 + number2;
+yalgavar = number1 - number2;
+urjver = number1 * number2;
+noogdvor = number1 / number2;
+console.log(niilber);
+console.log(yalgavar);
+console.log(urjver);
+console.log(noogdvor);
+
+let a = 3;
+let b = 4;
+let c = 5;
+p = a + b + c;
+console.log(p);
+
+let byte = "8 byte";
+let kilobyte = "1024 byte";
+let megabyte = "1024 kilobyte";
+megabyte = 1024 * 1024 * 8;
+console.log(15 * megabyte);
+
+let CDegree = 35;
+FDegree = CDegree + 32;
+console.log(FDegree);
+
+let FDegree = 69;
+CDegree = FDegree - 32;
+console.log(CDegree);
